3d9552c7a8d303be4dd3351702da1881afb9dd9d
Yavor Denys <denys.yavor@intapp.com>
2022-04-13 20:14:42 +0300

    Added word service

---
+++ EnglishWordHelperClient/Infrastructure/RequestFeatures/PaginationParameters.cs
@@ -0,0 +1,21 @@
+﻿namespace EnglishWordHelperClient.Infrastructure.RequestFeatures
+{
+    public class PaginationParameters
+    {
+        const int maxPageSize = 20;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 7;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+        public string SearchTerm { get; set; }
+    }
+}
